89cffc2fbe7475f6e0b329c0c68204f5a68f7ecf
dmzhari <harigrimorum990@gmail.com>
2024-03-08 08:25:07 +0700

    Update Readme (If you still don't understand then there is something in your brain that you need to unlearn)

--- inc/function.database.php
+++ inc/function.database.php
@@ -3,6 +3,12 @@ session_start();
 error_reporting(0);
 date_default_timezone_set('Asia/Jakarta');
 
+/**
+ * Localhost = Host Server
+ * Root = User DB
+ * Empty or Null = Password DB
+ * db_bkkmahardhika = Database Name
+ */
 $conn = new mysqli('localhost', 'root', '', 'db_bkkmahardhika');
 
 if ($conn->connect_error) {
